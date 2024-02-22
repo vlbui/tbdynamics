7197cc3ea3e906df12ede343b705a34572f311ed
longbui <longbui189@gmail.com>
2024-02-22 15:10:29 +1100

    Change add infection flow

--- tbdynamics/model.py
+++ tbdynamics/model.py
@@ -31,7 +31,7 @@ def build_model(
     birth_rates = get_birth_rate()
     death_rates = get_death_rate()
     death_df = process_death_rate(death_rates, age_strata, birth_rates.index)
-    initialize_model_conditions(model,add_triangular)
+    initialize_model_conditions(model, add_triangular)
     add_entry_flow(model, birth_rates)
     add_natural_death_flow(model)
     add_infection_flow(model)
@@ -39,27 +39,39 @@ def build_model(
     add_infect_death_flow(model)
     add_self_recovery_flow(model)
     stratify_model_by_age(
-        model, compartments, infectious_compartments, age_strata, death_df, fixed_params, matrix
+        model,
+        compartments,
+        infectious_compartments,
+        age_strata,
+        death_df,
+        fixed_params,
+        matrix,
     )
     request_model_outputs(
         model, compartments, latent_compartments, infectious_compartments, age_strata
     )
     return model
 
+
 def initialize_model_conditions(model, add_triangular):
     # Set the initial population with either 0 or 1 infectious individual(s)
     start_pop = Parameter("start_population_size")
     if add_triangular:
-        model.set_initial_population({
-            "infectious": 0,
-            "susceptible": start_pop - 0,
-        })
+        model.set_initial_population(
+            {
+                "infectious": 0,
+                "susceptible": start_pop - 0,
+            }
+        )
         seed_infectious(model)
     else:
-        model.set_initial_population({
-            "infectious": 1,
-            "susceptible": start_pop - 1,
-        })
+        model.set_initial_population(
+            {
+                "infectious": 1,
+                "susceptible": start_pop - 1,
+            }
+        )
+
 
 def add_entry_flow(model, birth_rates):
     process = "birth"
@@ -77,31 +89,24 @@ def add_natural_death_flow(model):
 
 def add_infection_flow(model):
     infection_flows = [
-        ("infection", "susceptible", "early_latent", "contact_rate"),
+        ("susceptible", None),
         (
-            "infection_from_latent",
             "late_latent",
-            "early_latent",
-            "contact_rate",
             "rr_infection_latent",
         ),
         (
-            "infection_from_recovered",
             "recovered",
-            "early_latent",
-            "contact_rate",
             "rr_infection_recovered",
         ),
     ]
-    for flow in infection_flows:
-        process, origin, destination, rate_param = flow[:4]  # Always present
-        modifier_param = flow[4] if len(flow) > 4 else None  # Optional parameter
-        rate = (
-            Parameter(rate_param) * Parameter(modifier_param)
-            if modifier_param
-            else Parameter(rate_param)
+    for origin, modifier in infection_flows:
+        process = f"infection_from_{origin}"
+        flow_rate = (
+            Parameter("contact_rate") * Parameter(modifier)
+            if modifier
+            else Parameter("contact_rate")
         )
-        model.add_infection_frequency_flow(process, rate, origin, destination)
+        model.add_infection_frequency_flow(process, flow_rate, origin, "early_latent")
 
 
 def add_latency_flow(model):
@@ -128,15 +133,26 @@ def add_infect_death_flow(model):
 
 
 def stratify_model_by_age(
-    model, compartments, infectious_compartments, age_strata, death_df ,fixed_params, matrix
+    model,
+    compartments,
+    infectious_compartments,
+    age_strata,
+    death_df,
+    fixed_params,
+    matrix,
 ):
     age_strat = get_age_strat(
-        compartments, infectious_compartments, age_strata, death_df,fixed_params, matrix
+        compartments,
+        infectious_compartments,
+        age_strata,
+        death_df,
+        fixed_params,
+        matrix,
     )
     model.stratify_with(age_strat)
 
 
-def get_age_strat(compartments, infectious, age_strata,death_df,fixed_params, matrix):
+def get_age_strat(compartments, infectious, age_strata, death_df, fixed_params, matrix):
     strat = AgeStratification("age", age_strata, compartments)
     strat.set_mixing_matrix(matrix)
     universal_death_funcs, death_adjs = {}, {}
@@ -169,61 +185,7 @@ def get_age_strat(compartments, infectious, age_strata,death_df,fixed_params, ma
             inf_adjs[str(age_low)] = Multiply(infectiousness)
 
         strat.add_infectiousness_adjustments(comp, inf_adjs)
-
     return strat
-
-
-def get_organ_strat(
-    infectious_compartments: list,
-    fixed_params: dict,
-):
-    ORGAN_STRATA = [
-        "smear_positive",
-        "smear_negative",
-        "extrapulmonary",
-    ]
-    strat = Stratification("organ", ORGAN_STRATA, infectious_compartments)
-
-    # Define infectiousness adjustment by organ status
-    inf_adj = {
-        organ: Multiply(fixed_params.get(f"{organ}_infect_multiplier", 1))
-        for organ in ORGAN_STRATA
-    }
-
-    for comp in infectious_compartments:
-        strat.add_infectiousness_adjustments(comp, inf_adj)
-
-    # Define different natural history (infection death) by organ status
-    infect_death_adjs = {
-        "smear_positive": Overwrite(Parameter("smear_positive_death_rate")),
-        "smear_negative": Overwrite(Parameter("smear_negative_death_rate")),
-        "extrapulmonary": Overwrite(Parameter("smear_negative_death_rate")),  
-    }
-    strat.set_flow_adjustments("infect_death", infect_death_adjs)
-
-    # Define different natural history (self recovery) by organ status
-    self_recovery_adjs = {
-        organ: Overwrite(Parameter(f"{organ}_self_recovery"))
-        for organ in ORGAN_STRATA[:-1]  # Excluding 'extrapulmonary' for custom handling
-    }
-    # Handle 'extrapulmonary' case if its adjustment is different from the others
-    self_recovery_adjs["extrapulmonary"] = Overwrite(Parameter("smear_negative_self_recovery"))
-    strat.set_flow_adjustments("self_recovery", self_recovery_adjs)
-
-    # Adjust the progression rates by organ using the requested incidence proportions
-    splitting_proportions = {
-        "smear_positive": fixed_params["incidence_props_pulmonary"]
-        * fixed_params["incidence_props_smear_positive_among_pulmonary"],
-        "smear_negative": fixed_params["incidence_props_pulmonary"]
-        * (1.0 - fixed_params["incidence_props_smear_positive_among_pulmonary"]),
-        "extrapulmonary": 1.0 - fixed_params["incidence_props_pulmonary"],
-    }
-    print(splitting_proportions)
-    for flow_name in ["early_activation", "late_activation"]:
-        flow_adjs = {k: Multiply(v) for k, v in splitting_proportions.items()}
-        strat.set_flow_adjustments(flow_name, flow_adjs)
-    return strat
-
 
 def seed_infectious(model: CompartmentalModel):
     """Seed infectious.
