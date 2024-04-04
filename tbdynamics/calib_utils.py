be5dcf27bb898a984cd299dcf6ec87f3d8f5636e
longbui <longbui189@gmail.com>
2024-04-05 01:31:32 +1100

    April calibration

--- tbdynamics/calib_utils.py
+++ tbdynamics/calib_utils.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-from plotly.subplots import make_subplots
+
 import plotly.io as pio
 from typing import List, Dict
 from tbdynamics.model import build_model
@@ -19,9 +19,9 @@ from tbdynamics.constants import (
     infectious_compartments,
     indicator_names
 )
-from tbdynamics.utils import get_row_col_for_subplots, get_standard_subplot_fig, get_target_from_name, round_sigfig
-from tbdynamics.constants import PLOT_START_DATE, PLOT_END_DATE, indicator_names
-pio.templates.default = "simple_white"
+from tbdynamics.utils import get_row_col_for_subplots, get_standard_subplot_fig
+from tbdynamics.constants import indicator_names
+pio.templates.default = "plotly_white"
 
 
 
@@ -60,21 +60,21 @@ def get_all_priors() -> list:
         All the priors used under any analyses
     """
     return [
-        # esp.UniformPrior("start_population_size", (2000000.0, 2500000.0)),
+        esp.UniformPrior("start_population_size", (2000000.0, 2500000.0)),
         esp.UniformPrior("contact_rate", (0.005, 0.015)),
         esp.UniformPrior("rr_infection_latent", (0.2, 0.5)),
         esp.UniformPrior("rr_infection_recovered", (0.2, 0.5)),
         esp.UniformPrior("progression_multiplier", (1.0, 2.0)),
-        # esp.UniformPrior("seed_time", (1800.0, 1840.0)),
-        # esp.UniformPrior("seed_num", (1.0, 100.00)),
-        # esp.UniformPrior("seed_duration", (1.0, 5.0)),
+        esp.UniformPrior("seed_time", (1800.0, 1840.0)),
+        esp.UniformPrior("seed_num", (1.0, 100.00)),
+        esp.UniformPrior("seed_duration", (1.0, 20.0)),
         esp.UniformPrior("smear_positive_death_rate", (0.335, 0.449)),
         esp.UniformPrior("smear_negative_death_rate", (0.017, 0.035)),
         esp.UniformPrior("smear_positive_self_recovery", (0.177, 0.288)),
         esp.UniformPrior("smear_negative_self_recovery", (0.073, 0.209)),
-        # esp.UniformPrior("screening_scaleup_shape", (0.07, 0.1)),
-        # esp.UniformPrior("screening_inflection_time", (1993, 2005)),
-        # esp.UniformPrior("screening_end_asymp", (0.5, 0.60)),
+        esp.UniformPrior("screening_scaleup_shape", (0.05, 0.15)),
+        esp.UniformPrior("screening_inflection_time", (1990, 2010)),
+        esp.UniformPrior("screening_end_asymp", (0.55, 0.63)),
     ]
 
 
@@ -98,14 +98,17 @@ def get_targets() -> list:
         est.NormalTarget("notification", target_data["notification"], stdev=10.0),
         est.NormalTarget("percentage_latent", target_data["percentage_latent"], 1.0),
         est.NormalTarget("prevalence_pulmonary", target_data["prevalence_pulmonary"], 1.0),
-        est.NormalTarget("incidence", target_data["incidence"], 1.0)
+        est.NormalTarget("incidence", target_data["incidence"], 1.0),
+        #est.NormalTarget("cdr", target_data["cdr"], 0.1)
     ]
 
 def plot_spaghetti(
     spaghetti: pd.DataFrame, 
     indicators: List[str], 
     n_cols: int, 
-    targets: list,
+    target_data: Dict,
+    plot_start_date: int = 1800,
+    plot_end_date: int = 2023
 ) -> go.Figure:
     """Generate a spaghetti plot to compare any number of requested outputs to targets.
 
@@ -118,8 +121,8 @@ def plot_spaghetti(
     Returns:
         The spaghetti plot figure object
     """
-    rows = int(np.ceil(len(indicators) / n_cols))
-    fig = get_standard_subplot_fig(rows, n_cols, [(indicator_names[ind] if ind in indicator_names else ind.replace('_', ' ')) for ind in indicators])
+    nrows = int(np.ceil(len(indicators) / n_cols))
+    fig = get_standard_subplot_fig(nrows, n_cols, [(indicator_names[ind] if ind in indicator_names else ind.replace('_', ' ')) for ind in indicators])
     for i, ind in enumerate(indicators):
         row, col = get_row_col_for_subplots(i, n_cols)
 
@@ -129,21 +132,24 @@ def plot_spaghetti(
         fig.add_traces(px.line(ind_spagh).data, rows=row, cols=col)
 
         # Targets
-        target = get_target_from_name(targets, ind)
-        target_marker_config = dict(size=5.0, line=dict(width=0.5, color='DarkSlateGrey'))
-        lines = go.Scatter(x=target.index, y=target, marker=target_marker_config, name='targets', mode='markers')
-        fig.add_trace(lines, row=row, col=col)
+        if ind in target_data.keys():
+            target = target_data[ind]
+            target_marker_config = dict(size=5.0, line=dict(width=0.5, color='DarkSlateGrey'))
+            lines = go.Scatter(x=target.index, y=target, marker=target_marker_config, name='targets', mode='markers')
+            fig.add_trace(lines, row=row, col=col)
         fig.update_layout(yaxis4=dict(range=[0, 2.2]))
         fig.update_layout(showlegend=False)
         fig.update_yaxes(range=None)
-    return fig.update_xaxes(range=[PLOT_START_DATE, PLOT_END_DATE])
+    return fig.update_xaxes(range=[plot_start_date, plot_end_date])
 
 def plot_output_ranges(
     quantile_outputs: Dict[str, pd.DataFrame], 
-    targets: list, 
-    outputs: List[str], 
+    target_data: Dict, 
+    indicators: List[str], 
     quantiles: List[float], 
     n_cols: int,
+    plot_start_date: int = 1800,
+    plot_end_date: int = 2023,
     max_alpha: float=0.7
 ) -> go.Figure:
     """Plot the credible intervals with subplots for each output,
@@ -160,22 +166,19 @@ def plot_output_ranges(
     Returns:
         The interactive figure
     """
-    titles = [(indicator_names[ind] if ind in indicator_names else ind.replace('_', ' ')) for ind in outputs]
-    fig = get_standard_subplot_fig(1, n_cols, titles)
-    for i, output in enumerate(outputs):
+    nrows = int(np.ceil(len(indicators) / n_cols))
+    fig = get_standard_subplot_fig(nrows, n_cols, [(indicator_names[ind] if ind in indicator_names else ind.replace('_', ' ')) for ind in indicators])
+    for i, ind in enumerate(indicators):
         row, col = get_row_col_for_subplots(i, n_cols)
-        data = quantile_outputs[output]
+        data = quantile_outputs[ind]
         for q, quant in enumerate(quantiles):
             alpha = min((q, len(quantiles) - q)) / np.floor(len(quantiles) / 2) * max_alpha
             fill_colour = f'rgba(0,30,180,{str(alpha)})'
             fig.add_traces(go.Scatter(x=data.index, y=data[quant], fill='tonexty', fillcolor=fill_colour, line={'width': 0}, name=quant), rows=row, cols=col)
         fig.add_traces(go.Scatter(x=data.index, y=data[0.5], line={'color': 'black'}, name='median'), rows=row, cols=col)
-        if output in [t.name for t in targets]:
-            target = get_target_from_name(targets, output)
+        if ind in target_data.keys():
+            target = target_data[ind]
             marker_format = {'size': 5.0, 'color': 'rgba(250, 135, 206, 0.2)', 'line': {'width': 1.0}}
             fig.add_traces(go.Scatter(x=target.index, y=target, mode='markers', marker=marker_format, name=target.name), rows=row, cols=col)
-    fig.update_xaxes(range=[PLOT_START_DATE, PLOT_END_DATE])
+    fig.update_xaxes(range=[plot_start_date, plot_end_date])
     return fig.update_layout(yaxis4={'range': [0.0, 2.5]}, showlegend=False)
-
-
-
