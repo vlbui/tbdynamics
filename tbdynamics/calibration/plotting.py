01dd3d47a8060dc3178815c49a83e457f9d29880
longbui <longbui189@gmail.com>
2024-09-03 02:55:08 +1000

    Add yearly cumsum

--- tbdynamics/calibration/plotting.py
+++ tbdynamics/calibration/plotting.py
@@ -5,14 +5,12 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 import plotly.io as pio
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 from typing import List, Dict
 
 from tbdynamics.constants import indicator_names, quantiles
 from tbdynamics.utils import get_row_col_for_subplots, get_standard_subplot_fig
 from tbdynamics.constants import indicator_names, scenario_names
-from .utils import convert_prior_to_numpyro
+
 
 # Define the custom template
 extended_layout = pio.templates["simple_white"].layout
@@ -290,119 +288,6 @@ def plot_output_ranges(
     return fig
 
 
-def plot_post_prior_comparison(idata, priors, params_name):
-    """
-    Plot comparison of model posterior outputs against priors.
-
-    Args:
-        idata: Arviz inference data from calibration.
-        priors: Dictionary of custom prior objects.
-        params_name: Dictionary mapping parameter names to descriptive titles.
-
-    Returns:
-        The figure object.
-    """
-    # Filter priors to exclude those containing '_dispersion'
-    req_vars = [var for var in priors.keys() if "_dispersion" not in var]
-    num_vars = len(req_vars)
-    num_rows = (num_vars + 1) // 2  # Ensure even distribution across two columns
-
-    fig, axs = plt.subplots(num_rows, 2, figsize=(10, 5 * num_rows))
-    axs = axs.ravel()
-
-    for i_ax, ax in enumerate(axs):
-        if i_ax < num_vars:
-            var_name = req_vars[i_ax]
-            posterior_samples = idata.posterior[var_name].values.flatten()
-            low_post = np.min(posterior_samples)
-            high_post = np.max(posterior_samples)
-            x_vals_posterior = np.linspace(low_post, high_post, 100)
-
-            numpyro_prior, prior_bounds = convert_prior_to_numpyro(priors[var_name])
-            if prior_bounds:
-                low_prior, high_prior = prior_bounds
-                x_vals_prior = np.linspace(low_prior, high_prior, 100)
-            else:
-                x_vals_prior = (
-                    x_vals_posterior  # Fallback if no specific prior bounds are given
-                )
-
-            # Compute the original prior density using NumPy's exp function
-            prior_density = np.exp(numpyro_prior.log_prob(x_vals_prior))
-
-            # Compute the posterior density using a kernel density estimate
-            posterior_density = np.histogram(posterior_samples, bins=100, density=True)[
-                0
-            ]
-            x_vals_posterior = np.linspace(low_post, high_post, len(posterior_density))
-
-            ax.fill_between(
-                x_vals_prior,
-                prior_density,
-                color="k",
-                alpha=0.2,
-                linewidth=2,
-                label="Prior",
-            )
-            ax.plot(
-                x_vals_posterior,
-                posterior_density,
-                color="b",
-                linewidth=1,
-                linestyle="solid",
-                label="Posterior",
-            )
-
-            # Set the title using the descriptive name from params_name
-            title = params_name.get(
-                var_name, var_name
-            )  # Use var_name if not in params_name
-            ax.set_title(title)
-            ax.legend()
-        else:
-            ax.axis("off")  # Turn off empty subplots if the number of req_vars is odd
-
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_trace(idata: az.InferenceData, params_name: dict):
-    """
-    Plot trace plots for the InferenceData object, excluding parameters containing '_dispersion'.
-    Adds descriptive titles from `params_name`.
-
-    Args:
-        idata: InferenceData object from ArviZ containing calibration outputs.
-        params_name: Dictionary mapping parameter names to descriptive titles.
-    """
-    # Filter out parameters containing '_dispersion'
-    filtered_posterior = idata.posterior.drop_vars(
-        [var for var in idata.posterior.data_vars if "_dispersion" in var]
-    )
-
-    # Plot trace plots with the filtered parameters
-    trace_fig = az.plot_trace(
-        filtered_posterior, figsize=(16, 3.1 * len(filtered_posterior.data_vars))
-    )
-
-    # Set titles for each row of plots
-    var_names = list(
-        filtered_posterior.data_vars.keys()
-    )  # Get the list of variable names
-    for i, var_name in enumerate(var_names):
-        row_axes = trace_fig[i, :]  # Get the axes in the current row
-        title = params_name.get(
-            var_name, var_name
-        )  # Get the title from params_name or default to var_name
-        row_axes[0].set_title(
-            title, fontsize=14, loc="center"
-        )  # Set title for the first column
-        row_axes[1].set_title("")  # Clear the title for the second column
-
-    plt.tight_layout()
-    plt.show()
-
-
 def plot_outputs_for_covid(
     scenario_outputs: Dict[str, Dict[str, pd.DataFrame]],
     target_data: Dict[str, pd.Series],
@@ -540,7 +425,7 @@ def plot_outputs_for_covid(
     return fig
 
 
-def plot_covid_scenarios_comparison(
+def plot_covid_configs_comparison(
     diff_quantiles, indicators, years, plot_type="abs", n_cols=1
 ):
     """
@@ -663,118 +548,8 @@ def plot_covid_scenarios_comparison(
     return fig
 
 
-def plot_detection_scenario_comparison_box(diff_quantiles, indicators, plot_type="abs"):
-    """
-    Plot the quantile differences for given indicators across multiple scenarios.
-
-    Args:
-        diff_quantiles (dict): The quantile difference data structured as a dictionary.
-        indicators (list): List of indicators to plot (e.g., 'cumulative_diseased', 'cumulative_deaths').
-        plot_type (str): "abs" for absolute differences, "rel" for relative differences.
-
-    Returns:
-        fig: A Plotly figure object.
-    """
-    nrows = len(indicators)
-    colors = px.colors.qualitative.Plotly
-    indicator_colors = {
-        ind: colors[i % len(colors)] for i, ind in enumerate(indicators)
-    }
-
-    fig = go.Figure()
-
-    for i, indicator in enumerate(list(indicators)):
-        color = indicator_colors.get(indicator, "rgba(0, 123, 255)")
-
-        # Extract data for the given indicator and plot_type
-        scenarios = list(diff_quantiles.keys())  # Extract scenario names
-        medians = []
-        lower_errors = []
-        upper_errors = []
-
-        for scenario in scenarios:
-            median_val = diff_quantiles[scenario][plot_type][indicator].loc[
-                2035.0, 0.500
-            ]
-            lower_val = diff_quantiles[scenario][plot_type][indicator].loc[
-                2035.0, 0.025
-            ]
-            upper_val = diff_quantiles[scenario][plot_type][indicator].loc[
-                2035.0, 0.975
-            ]
-
-            medians.append(median_val)
-            lower_errors.append(median_val - lower_val)
-            upper_errors.append(upper_val - median_val)
-
-        # Add trace for this indicator in the specified order
-        fig.add_trace(
-            go.Bar(
-                y=[
-                    scenario_names.get(
-                        scenario, scenario.replace("_", " ").capitalize()
-                    )
-                    for scenario in scenarios
-                ],  # Descriptive scenario names
-                x=medians,  # Median values on x-axis
-                orientation="h",
-                marker=dict(color=color),
-                error_x=dict(
-                    type="data",
-                    symmetric=False,
-                    array=upper_errors,  # Upper bound error
-                    arrayminus=lower_errors,  # Lower bound error
-                    color="black",  # Black color for error bars
-                    thickness=1.5,  # Thicker error bars
-                    width=3,  # Wider error bars
-                ),
-                name=indicator.replace(
-                    "_", " "
-                ).capitalize(),  # Use indicator name for legend
-            )
-        )
-
-    # Ensure traces are ordered according to indicators list
-    fig.data = sorted(
-        fig.data,
-        key=lambda trace: indicators.index(trace.name.lower().replace(" ", "_")),
-    )
-
-    # Update layout with tight margins and ordered legend
-    fig.update_layout(
-        title={"text": "Reference: Status-quo scenario", "x": 0.5},
-        xaxis_title="",
-        yaxis_title="",
-        barmode="group",
-        height=320,  # Adjust height based on the number of rows
-        margin=dict(
-            l=20, r=5, t=40, b=40
-        ),  # Tight layout with more bottom margin for legend
-        yaxis=dict(
-            tickangle=-45,  # Rotate y-axis labels by 45 degrees
-            categoryorder="array",  # Ensure the order follows the scenarios
-            categoryarray=[
-                scenario_names.get(scenario, scenario.replace("_", " ").capitalize())
-                for scenario in reversed(scenarios)
-            ],
-        ),
-        legend=dict(
-            title="",
-            orientation="h",
-            yanchor="bottom",
-            y=-0.3,  # Position the legend below the plot
-            xanchor="center",
-            x=0.5,
-            itemsizing="constant",  # Consistent item sizing
-            traceorder="normal",  # Keep the legend order as per the traces added
-        ),
-    )
-
-    return fig
-
-
 def plot_covid_scenarios_comparison_combined(
-    diff_quantiles, indicators, years, plot_type="abs", n_cols=1
+    diff_quantiles, plot_type="abs"
 ):
     """
     Plot the median differences with error bars indicating the range from 0.025 to 0.975 quantiles
@@ -782,16 +557,16 @@ def plot_covid_scenarios_comparison_combined(
 
     Args:
         diff_quantiles: A dictionary containing the calculated quantile differences (output from `calculate_diff_quantiles`).
-        indicators: List of indicators to plot.
-        years: List of years for which to plot the data.
         plot_type: "abs" for absolute differences, "rel" for relative differences.
-        n_cols: (Deprecated) No longer relevant since all indicators are plotted in one plot.
 
     Returns:
         A Plotly figure with all indicators plotted together, each containing horizontal bars for multiple years.
     """
     fig = go.Figure()
     colors = px.colors.qualitative.Plotly
+    indicators = diff_quantiles[plot_type].keys()
+    years = diff_quantiles[plot_type][list(indicators)[0]].index
+
     indicator_colors = {
         ind: colors[i % len(colors)] for i, ind in enumerate(indicators)
     }
@@ -800,11 +575,6 @@ def plot_covid_scenarios_comparison_combined(
         color = indicator_colors.get(
             ind, "rgba(0, 123, 255)"
         )  # Default to blue if not specified
-
-        if not all(year in diff_quantiles[plot_type][ind].index for year in years):
-            raise ValueError(
-                f"Some years are missing in the index for indicator: {ind}"
-            )
 
         median_diffs = []
         lower_diffs = []
@@ -820,7 +590,7 @@ def plot_covid_scenarios_comparison_combined(
                 y=[str(int(year)) for year in years],  # Convert years to strings
                 x=median_diffs,  # Median differences
                 orientation="h",
-                name=indicator_names.get(ind, ind.replace("_", " ").capitalize()),
+                name=ind.replace("_", " ").capitalize(),
                 marker=dict(color=color),
                 error_x=dict(
                     type="data",
@@ -844,7 +614,6 @@ def plot_covid_scenarios_comparison_combined(
         title={
             "text": "Reference: Counterfactual no COVID-19",
             "x": 0.5,
-            # 'y': 0.95,
             "xanchor": "center",
             "yanchor": "top",
         },
@@ -1245,56 +1014,103 @@ def plot_scenario_output_ranges_by_col(
 
     return fig
 
-
-def tabulate_calib_results(
-    idata: az.data.inference_data.InferenceData, params_name
-) -> pd.DataFrame:
+def plot_detection_scenarios_comparison_box(
+    detection_diff_results: Dict[str, Dict[str, pd.DataFrame]],
+    plot_type: str ="abs", 
+    year: int =2035
+):
     """
-    Get tabular outputs from calibration inference object,
-    except for the dispersion parameters, and standardize formatting.
+    Plot the median differences with error bars indicating the range from 0.025 to 0.975 quantiles
+    for the cumulative diseased and deaths across multiple detection scenarios for the specified year.
 
     Args:
-        idata: InferenceData object from ArviZ containing calibration outputs.
-        priors: List of parameter names as strings.
+        detection_diff_results: A dictionary containing the calculated quantile differences (output from `calculate_scenario_and_diff_outputs`).
+        plot_type: "abs" for absolute differences, "rel" for relative differences.
+        year: The year for which to plot the data (default is 2035).
+        scenario_names: Optional dictionary to map scenario keys to more descriptive names.
 
     Returns:
-        Calibration results table in standard format.
+        A Plotly figure with all indicators plotted together for the specified year.
     """
-    # Generate summary table
-    table = az.summary(idata)
+    fig = go.Figure()
+    colors = px.colors.qualitative.Plotly
+    indicators = ["cumulative_diseased", "cumulative_deaths"]
+    scenarios = detection_diff_results.keys()
 
-    # Filter out dispersion parameters
-    table = table[~table.index.str.contains("_dispersion")]
+    indicator_colors = {
+        ind: colors[i % len(colors)] for i, ind in enumerate(indicators)
+    }
 
-    # Round and format the relevant columns
-    for col_to_round in [
-        "mean",
-        "sd",
-        "hdi_3%",
-        "hdi_97%",
-        "ess_bulk",
-        "ess_tail",
-        "r_hat",
-    ]:
-        table[col_to_round] = table.apply(
-            lambda x: str(round(x[col_to_round], 3)), axis=1
-        )
+    for scenario in scenarios:
+        for ind in indicators:
+            color = indicator_colors.get(
+                ind, "rgba(0, 123, 255)"
+            )  # Default to blue if not specified
 
-    # Create the HDI column
-    table["hdi"] = table.apply(lambda x: f'{x["hdi_3%"]} to {x["hdi_97%"]}', axis=1)
+            quantile_data = detection_diff_results[scenario][plot_type][ind].loc[year]
+            median_diff = round(quantile_data[0.5])
+            lower_diff = round(quantile_data[0.025])
+            upper_diff = round(quantile_data[0.975])
 
-    # Drop unnecessary columns
-    table = table.drop(["mcse_mean", "mcse_sd", "hdi_3%", "hdi_97%"], axis=1)
+            fig.add_trace(
+                go.Bar(
+                    y=[
+                        scenario_names.get(
+                            scenario, scenario.replace("_", " ").capitalize()
+                        )
+                        for scenario in scenarios
+                    ],  # Map scenario keys to descriptive names
+                    x=[median_diff],  # Median difference
+                    orientation="h",
+                    name=f"{ind.replace('_', ' ').capitalize()}",
+                    marker=dict(color=color),
+                    error_x=dict(
+                        type="data",
+                        symmetric=False,
+                        array=[upper_diff - median_diff],
+                        arrayminus=[median_diff - lower_diff],
+                        color="black",
+                        thickness=1.5,
+                        width=3,
+                    ),
+                )
+            )
 
-    # Rename columns for standardized format
-    table.columns = [
-        "Mean",
-        "Standard deviation",
-        "ESS bulk",
-        "ESS tail",
-        "\\textit{\^{R}}",
-        "High-density interval",
-    ]
-    table.index = table.index.map(lambda x: params_name.get(x, x))
-    table.index.name = "Parameter"
-    return table
+    # Ensure traces are ordered according to indicators list
+    fig.data = sorted(
+        fig.data,
+        key=lambda trace: indicators.index(trace.name.lower().replace(" ", "_")),
+    )
+
+    # Update layout with tight margins and ordered legend
+    fig.update_layout(
+        title={"text": "Reference: Status-quo scenario", "x": 0.5},
+        xaxis_title="",
+        yaxis_title="",
+        barmode="group",
+        height=320,  # Adjust height based on the number of rows
+        margin=dict(
+            l=20, r=5, t=40, b=40
+        ),  # Tight layout with more bottom margin for legend
+        yaxis=dict(
+            tickangle=-45,  # Rotate y-axis labels by 45 degrees
+            categoryorder="array",  # Ensure the order follows the scenarios
+            categoryarray=[
+                scenario_names.get(scenario, scenario.replace("_", " ").capitalize())
+                for scenario in reversed(scenarios)
+            ],
+        ),
+        legend=dict(
+            title="",
+            orientation="h",
+            yanchor="bottom",
+            y=-0.3,  # Position the legend below the plot
+            xanchor="center",
+            x=0.5,
+            itemsizing="constant",  # Consistent item sizing
+            traceorder="normal",  # Keep the legend order as per the traces added
+        ),
+    )
+
+    return fig
+
