0ac228c0e51e8fa3e2853b953488b1387f057147
longbui <longbui189@gmail.com>
2025-07-08 15:48:09 +1000

    Clearing code

--- tbdynamics/camau/strats/act3.py
+++ tbdynamics/camau/strats/act3.py
@@ -41,16 +41,19 @@ def get_act3_strat(
 
     # Incorporate the screening rates
     act_trial_screening_rate = {
-        2014.0: 0.0,
-        2015.0: 2.0,
-        2016.0: 2.0,
-        2017.0: 1.97,
-        2018.0: 1.50,
+        2014.9: 0.0,
+        2015.0: 1.51,
+        2015.9: 1.51,
+        2016.0: 1.15,
+        2016.9: 1.15,
+        2017.0: 0.97,
+        2017.9: 0.97,
+        2018.0: 0.80,
         2018.1: 0.00,
     }
     act_trial_screening_rate = get_interpolation_rates_from_annual(act_trial_screening_rate)
     act_control_screening_rate = {
-        2017.0: 0.0,
+        2017.9: 0.0,
         2018.0: 1.8,
         2018.1: 0.0,
     }
