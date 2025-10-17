10a66ca50f33b388cf5aa18f654d1bcdd73b2580
vlbui <lvbui189@gmail.com>
2025-10-17 08:08:46 +0700

    Cleaning

--- tbdynamics/camau/calibration/utils.py
+++ tbdynamics/camau/calibration/utils.py
@@ -68,7 +68,7 @@ def get_all_priors(covid_effects: Optional[Dict[str, bool]]) -> List:
         esp.BetaPrior("rr_infection_latent", 3.0, 5.0),
         esp.BetaPrior("rr_infection_recovered", 2.5, 4.5),
         esp.TruncNormalPrior("early_prop_adjuster", 0, 0.05, (-2.0, 2.0)),
-        esp.UniformPrior("late_reactivation_adjuster", (0.5, 5.0)),
+        esp.GammaPrior.from_mode("late_reactivation_adjuster", 1.0, 2.0),
         esp.GammaPrior.from_mean("clearance_rate", 0.025, 0.1),
         # esp.BetaPrior("igra_positive_among_cleared_prop", 6.0, 4.0),
         esp.TruncNormalPrior(
@@ -110,11 +110,6 @@ def get_targets() -> List[est.NormalTarget]:
     target_data = load_targets(CM_PATH / "targets.yml")
 
     return [
-        est.NormalTarget(
-            "total_population",
-            target_data["total_population"],
-            esp.UniformPrior("total_population_dispersion", (500.0, 5000.0)),
-        ),
         est.NormalTarget(
             "log_notification",
             np.log(target_data["notification"]),
